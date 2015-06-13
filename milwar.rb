9df85f23bb4c1dcd55b9cd80d1021ed409b74396
shimobayashi <akimasa@shimobayashi.name>
2015-06-13 17:06:35 +0900

    時間帯によって照明の色を変えるようにした

--- milwar.rb
+++ milwar.rb
@@ -8,11 +8,19 @@ settings = JSON.parse(STDIN.read)
 p settings
 
 cmd = ""
+cmd2 = ""
 if settings['home'] == '0' # 外出中
   cmd = "\\x41\\x00\\x55" # 消灯
 else # 在宅中
   if settings['sleep'] == '0' # 寝てない
     cmd = "\\x42\\x00\\x55" # 点灯
+
+    t = Time.now
+    if (t.hour >= 21 || t.hour < 5)
+      cmd2 = "\\x40\\x90\\x55" # 暖色
+    else
+      cmd2 = "\\xC2\\x00\\x55" # 白色
+    end
   else # 寝てる
     cmd = "\\x41\\x00\\x55" # 消灯
   end
@@ -21,6 +29,12 @@ end
 p cmd
 if cmd
   p `bash -c 'echo -ne "#{cmd}" > /dev/udp/192.168.10.16/8899'`
+  p $?
+end
+p cmd2
+if cmd2
+  sleep 0.1
+  p `bash -c 'echo -ne "#{cmd2}" > /dev/udp/192.168.10.16/8899'`
   p $?
 end
 
